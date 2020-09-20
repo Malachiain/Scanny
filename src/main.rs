3d8564c51a9a614a9b0ffaeddb4ab1e29bdb9227
Iain Lumsden <iain.a.lumsden@gmail.com>
2020-09-20 18:59:10 +1200

    start of printing out to a file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use anyhow::{ Result};
 use structopt::StructOpt;
 use std::net::{ TcpStream};
+use std::path::PathBuf;
+use std::ops::Range;
 
 #[derive(StructOpt)]
 struct Cli {
@@ -8,27 +10,43 @@ struct Cli {
     from: u16 ,
     #[structopt(short = "t", long = "to", help = "the port to finish scanning at", default_value = "65535")]
     to: u16 ,
-
+    #[structopt(help = "Output file, stdout if not present", parse(from_os_str))]
+    output: Option<PathBuf>
 }
 
 const IP_ADDRESS: &str =  "127.0.0.1";
 
 fn main() -> Result<()>  {
     let args = Cli::from_args();
-    let scan_range = std::ops::Range { start: args.from, end: args.to };
+    let scan_range = Range { start: args.from, end: args.to };
+    match args.output {
+        Some(file_path) => scan_to_outfile(file_path),
+        None => scan_to_sysout(scan_range)
+    }
+    Ok(())
+}
+
+fn scan_to_sysout(scan_range: Range<u16>){
     println!("{:10}|{:5}|{}", "port", "open", "service");
     println!("{:-<25}","");
-
     for p in scan_range {
         let is_open = is_port_open(IP_ADDRESS, p);
         if is_open {
             println!("{:<10}|{:5}|{}", p, is_open, "NA");
         }
     }
-    
+} 
+
+fn scan_to_outfile(outpath: PathBuf) {
+    match outpath.to_str() {
+        Some(s) =>  println!("file path {}", s),
+        None => println!("couldn't convert to sting")
+
+    }
    
-    Ok(())
 }
+
+
 
 fn is_port_open(ip_address: &str, port: u16) -> bool {
     match TcpStream::connect((ip_address, port)) {
